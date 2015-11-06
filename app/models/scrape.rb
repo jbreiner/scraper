1ae38eaaea0687e626f1a39bb363ce4fed66cd40
John Breiner <jbreiner@gmail.com>
2015-11-06 15:55:45 -0600

    refine scrape

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -13,10 +13,28 @@ class Scrape
 			self.director = doc.at("//td[@itemprop = 'director']").css('a').first.text 
 			self.genre = doc.at("//span[@itemprop = 'genre']").text 
 			self.runtime = doc.at("//time[@itemprop = 'duration']").text 
-			self.synopsis = doc.css('#movieSynopsis').text 
+			s = doc.css('#movieSynopsis').text
+			if ! s.valid_encoding?
+				s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
+			end
+			self.synopsis = s
 			return true
 		rescue Exception => e
 			self.failure = "Something went wrong with the scrape"
 		end
+	end
+
+	def save_movie
+		movie = Movie.new(
+			title: self.title,
+			hotness: self.hotness,
+			image_url: self.image_url,
+			synopsis: self.synopsis,
+			rating: self.rating,
+			genre: self.genre,
+			director: self.director,
+			runtime: self.runtime
+			)
+		movie.save
 	end
 end
